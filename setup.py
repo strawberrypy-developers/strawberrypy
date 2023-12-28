d04b42fe0fdd09010465b456cf8aa077baa80af7
Nicolas Baù <nicolas.bau99@gmail.com>
2023-12-28 15:33:29 +0000

    Move setup parameters to pyproject and now build should ignore empty docstrings

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
 import setuptools
 
-setuptools.setup(
-    name='strawberrypy',
-    version='0.2.0', 
-    author='Roberta Favata and Nicolas Baù and Antimo Marrazzo',   
-    author_email='favata.roberta@gmail.com',
-    description='Python package for calculation of topological invariants through single-point formulas and local markers',
-    license='MIT License',
-    packages=setuptools.find_packages(),
-    install_requires=['pythtb==1.8.0',
-                      'tbmodels==1.4.3',
-                      'opt-einsum==3.3.0',],
-)
+setuptools.setup()
